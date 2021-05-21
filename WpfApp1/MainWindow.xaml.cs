89661d2dfa1381bbf126d0ea1b7315fde34eb72c
iKraxteNi <S!erp!en!49991>
2021-05-21 13:00:00 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        long w;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Suma_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float a = float.Parse(textBoxa.Text);
+                float b = float.Parse(textBoxb.Text);
+                
+                TextBoxw.Text = (a+b).ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj liczby");
+                throw;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float a = float.Parse(textBoxa.Text);
+                float b = float.Parse(textBoxb.Text);
+
+                TextBoxw.Text = (a - b).ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj liczby");
+                throw;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float a = float.Parse(textBoxa.Text);
+                float b = float.Parse(textBoxb.Text);
+
+                TextBoxw.Text = (a * b).ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj liczby");
+                throw;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                double a = float.Parse(textBoxa.Text);
+                double b = float.Parse(textBoxb.Text);
+                if (b == 0)
+                {
+                    TextBoxw.Text = ("nie dzielimy przez 0");
+
+                }
+                else
+                TextBoxw.Text = (a / b).ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj liczby");
+                throw;
+            }
+        }
+    }
+}
